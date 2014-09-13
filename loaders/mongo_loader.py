1085c20117515cbb660c912a65a39793274fa9af
nicholac <kris_nich@hotmail.co.uk>
2014-09-13 11:11:31 +0100

    Added First data dump

--- loaders/mongo_loader.py
+++ loaders/mongo_loader.py
@@ -6,6 +6,7 @@ Created on 9 Sep 2014
 Loader for sample data --> Mongo
 '''
 from pymongo import MongoClient
+from random import randint
 import csv
 
 def toMongo(inFile):
@@ -23,43 +24,52 @@ def toMongo(inFile):
     fh.close()
     #Fire at Mongo
     cnt = 0
-    print 'Running'
-    with open(inFile, 'rb') as csvfile:
-        readr = csv.reader(csvfile, delimiter=',')
-        for row in readr:
-            if cnt == 0:
-                #TODO: Read in the keys auto
-                pass
-            else:
-                doc = {'DH_CARD_ID':row[0],
-                        'BASKET_KEY':row[1],
-                        'TRANSACTION_DATE':row[2],
-                        'ITEM_QTY':int(row[3]),
-                        'WEIGHT_UOM_CODE':row[4],
-                        'WEIGHT_UOM_QTY':row[5],
-                        'NET_SPEND':float(row[6]),
-                        'VOLUME_UOM_CODE':row[7],
-                        'VOLUME_UOM_QTY':row[8],
-                        'TESCO_WEEK':int(row[9]),
-                        'TPNB':int(row[10]),
-                        'PROD_SUBGROUP':row[11],
-                        'PROD_GROUP':row[12],
-                        'STORE_FORMAT':row[13],
-                        'SLOYALTY_HIGH':row[14],
-                        'SLOYALTY_LOW':row[15]
-                       }
-                #Check for fruits & veg
-                match = []
-                for v in veg:
-                    if doc['PROD_GROUP'].lower() == v:
-                        print v
-                for f in fruits:
-                    if doc['PROD_GROUP'].lower() == f:
-                        print f
+    #number of times to run - randomised data
+    lps = 5
 
-                pid = coll.insert(doc)
-            doc = {}
-            cnt+=1
+    print 'Running'
+    for lp in lps:
+        print 'Duplicate Loop: ' + str(lp)
+        with open(inFile, 'rb') as csvfile:
+            readr = csv.reader(csvfile, delimiter=',')
+            for row in readr:
+                if cnt == 0:
+                    #TODO: Read in the keys auto
+                    pass
+                else:
+                    #1794234566
+                    DHCARDIDRnd = int(row[0])+randint(0,1000)
+                    #Date - 06-FEB-14
+                    
+                    doc = {'DH_CARD_ID':DHCARDIDRnd,
+                            'BASKET_KEY':row[1],
+                            'TRANSACTION_DATE':row[2],
+                            'ITEM_QTY':int(row[3]),
+                            'WEIGHT_UOM_CODE':row[4],
+                            'WEIGHT_UOM_QTY':row[5],
+                            'NET_SPEND':float(row[6]),
+                            'VOLUME_UOM_CODE':row[7],
+                            'VOLUME_UOM_QTY':row[8],
+                            'TESCO_WEEK':int(row[9]),
+                            'TPNB':int(row[10]),
+                            'PROD_SUBGROUP':row[11],
+                            'PROD_GROUP':row[12],
+                            'STORE_FORMAT':row[13],
+                            'SLOYALTY_HIGH':row[14],
+                            'SLOYALTY_LOW':row[15]
+                           }
+                    #Check for fruits & veg
+                    match = []
+                    for v in veg:
+                        if doc['PROD_GROUP'].lower() == v:
+                            print v
+                    for f in fruits:
+                        if doc['PROD_GROUP'].lower() == f:
+                            print f
+
+                    pid = coll.insert(doc)
+                doc = {}
+                cnt+=1
     print 'Done: '+str(cnt)
     client.disconnect()
 
